3af7147873d57e21c9bb896a2e31d3f488ffd449
ujahvee <ujahvictor666@gmail.com>
2022-03-13 12:41:21 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
