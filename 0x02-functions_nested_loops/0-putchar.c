f6516f1fc3c4b9072871e07ee3328f6324a834d7
ujahvee <ujahvictor666@gmail.com>
2022-03-15 15:37:13 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include <stdio.h?>
+#include <stdio.h>
 
 /*
- * main - Entry point
- * Return: Always 0 if (Success)
+ * main - Entry block
+ * Description: print holberton on new line   "holberton": Unkown word.
+ * Return: 0
  */
 int main(void)
 {
-	printf("_putchar,\n");
+	char c[5] = "_putchar";
+
+	int i = 0;
+
+	while (i < 5)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
