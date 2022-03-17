d7ab3365f554366804a9d682ff3a62434f0c1098
ujahvee <ujahvictor666@gmail.com>
2022-03-16 17:19:43 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,21 +9,21 @@
  */
 int print_sign(int n)
 {
-  int value;
+	int value;
 
-  if (n > 0)
-    {
-      value = 1;
-      _putchar('+');
-    }
-  else if (n == 0)
-    {
-      value = 0;
-      _putchar('0');
-    }
-  else
-    value = -1;
-  _putchar('-');
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+		value = -1;
+	_putchar('-');
 }
 
 return (value);
